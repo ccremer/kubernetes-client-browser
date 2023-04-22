a5392c357cae4f52e20d53396e6ca11e7d98d3da
ccremer <github.account@chrigel.net>
2023-04-22 10:33:10 +0200

    Add Secret type

--- packages/kubernetes-client-angular/projects/kubernetes-client-angular/src/lib/entities/core/metadata.ts
+++ packages/kubernetes-client-angular/projects/kubernetes-client-angular/src/lib/entities/core/metadata.ts
@@ -1,9 +1,13 @@
 import { EntityMetadataMap } from '@ngrx/data'
 import { ConfigMap } from '@ccremer/kubernetes-client/dist/types/core/ConfigMap'
 import { toNamespacedName } from './validator'
+import { Secret } from '@ccremer/kubernetes-client/dist/types/core/Secret'
 
 export const CoreEntityMetadataMap: EntityMetadataMap = {
   'v1/configmaps': {
     selectId: (configMap: ConfigMap) => toNamespacedName(configMap),
+  },
+  'v1/secrets': {
+    selectId: (secret: Secret) => toNamespacedName(secret),
   },
 }
